448ee776b2052dc8c76e1c9a0d3e6bb9eaf9029b
Stephen Greenberg <stephen.gberg@gmail.com>
2020-10-23 00:50:09 -0400

    reorganize

---
+++ tracker/include/ROOT_IO.hh
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <TTree.h>
+
+namespace ROOT_IO {
+
+	
+	
+};
--- tracker/tracking.cc
+++ tracker/tracking.cc
@@ -1,10 +1,8 @@
 #include <iostream>
-#include <TTree.h>
+#include "ROOT_IO.hh"
 
 int main(int argc, char *argv[]){
-	auto tree = new TTree("my_name", "testing");
+	
+	
 
-	std::cout << "made a tree" << std::endl;
-
-	delete tree;
-}
+} //main
