f0d22adb315d5c3ca1cc634c901757d8786ed5b5
Stephen Greenberg <stephen.gberg@gmail.com>
2021-05-18 18:20:01 -0400

    update plotting

--- analysis/analyzer.py
+++ analysis/analyzer.py
@@ -45,9 +45,9 @@ class H_mumu_Analyzer:
 					evt = self.Tree.Vertex_ErrorT[0]
 					for k in range(int(len(self.Tree.GenParticle_G4index))):
 						if self.Tree.GenParticle_G4index[k] > 0:
-							gen_x = self.Tree.GenParticle_x[k]/10.
+							gen_x = self.Tree.GenParticle_y[k]/10.
 							plotx.Fill( (vx-gen_x))#/evx )
-							gen_y = self.Tree.GenParticle_y[k]/10.
+							gen_y = self.Tree.GenParticle_x[k]/10.
 							ploty.Fill( (vy-gen_y))#/evy )
 							gen_z = self.Tree.GenParticle_z[k]/10.
 							plotz.Fill( (vz-gen_z))#/evz )
