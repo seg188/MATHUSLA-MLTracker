5cc13bf92d750716a76aa0830b39883707f05757
Stephen Greenberg <stephen.gberg@gmail.com>
2021-05-18 16:22:18 -0400

    update plotting

--- analysis/main.py
+++ analysis/main.py
@@ -9,7 +9,7 @@ import util
 if __name__ == "__main__":
 	directory = sys.argv[1]
 	h_mumu = analyzer.H_mumu_Analyzer(directory)
-	h_mumu.Analyze()
+	h_mumu.Plot()
     #klong = analyzer.K_Long_Analayzer(directory)
     #klong.Plot()
     #ev = event.Event(directory, 16303)
